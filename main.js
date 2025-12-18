d9fb6b3a07607a00fcd764fd6d03671ebe603f59
Nathi006 <khumalogiven08@gmail.com>
2025-12-18 23:54:16 +0200

    made improvements on the UI

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 const repo1 = "https://github.com/Kamogelobradley200/Shop-Mate";
 const repo2 = "https://github.com/StheshSphere/CareConnect";
+const linkedIn = "http://www.linkedin.com/in/nkosinathi-khumalo06";
+const github = "https://github.com/Nathi006";
 //
 const repo1Button = document.getElementById("project1");
 const repo2Button = document.getElementById("project2");
@@ -12,15 +14,14 @@ repo1Button.addEventListener("click", ()=>{
 repo2Button.addEventListener("click", ()=>{
     window.open(repo2);
 });
-//Theme changing
-const Theme = document.getElementById("themeBtn");
-Theme.addEventListener("click", ()=>{
-    if (Theme.textContent === "Light Mode"){
-        document.body.style.backgroundColor = "black";
-        Theme.textContent = "Dark Mode";
-    }
-    else{
-        document.body.style.backgroundColor = "white";
-        Theme.textContent = "Light Mode";
-    }
+//handle the connection buttons
+const link = document.getElementById("linked");//linkedIn button
+const git = document.getElementById("git");
+
+link.addEventListener("click", ()=>{
+    window.open(linkedIn);
+});
+
+git.addEventListener("click", ()=>{
+    window.open(github);
 });
